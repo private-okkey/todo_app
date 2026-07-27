4285d8e4b5df9b96a4055c2789fb54a146b9fb06
Ryookkey <Ryookkey@users.noreply.github.com>
2026-07-28 00:25:14 +0900

    add create show index about controller flash_message

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,15 +2,26 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      flash[:notice] = "タスクを作成しました"
+      redirect_to task_path(@task.id)
+    else
+      flash.now[:notice] = "タスク作成に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @tasks = current_user.tasks.order(created_at: :asc)
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
@@ -20,6 +31,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 
 end
